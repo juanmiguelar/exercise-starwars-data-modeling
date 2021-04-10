910cf99f16795bb085b0ffa630367af5fbcf1a93
Juan Miguel <juanmiguelar09@gmail.com>
2021-04-10 17:39:29 +0000

    Fin del modelo

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,63 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Planet(Base):
+    __tablename__ = 'planet'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    diameter = Column(Integer, nullable=True)
+    rotation_period = Column(Integer, nullable=True)
+    orbital_period = Column(Integer, nullable=True)
+    gravity = Column(Integer, nullable=True)
+    population = Column(Integer, nullable=True)
+    climate = Column(String(250), nullable=True)
+    terrain = Column(String(250), nullable=True)
+    surface_water = Column(Integer, nullable=True)
+    created = Column(DateTime, nullable=True)
+    edited = Column(DateTime, nullable=True)
+    name = Column(String(250), nullable=True)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    height = Column(Integer, nullable=True)
+    mass = Column(Integer, nullable=True)
+    hair_color = Column(String(250), nullable=True)
+    skin_color = Column(String(250), nullable=True)
+    eye_color = Column(String(250), nullable=True)
+    birth_year = Column(String(250), nullable=True)
+    gender = Column(String(250), nullable=True)
+    name = Column(String(250), nullable=True)
+    created = Column(DateTime, nullable=True)
+    edited = Column(DateTime, nullable=True)   
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    login = Column(String(250), nullable=True)
+    passw = Column(String(250), nullable=True)
+
+class Fav_Planet(Base):
+    __tablename__ = 'fav_planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+
+class Fav_Character(Base):
+    __tablename__ = 'fav_character'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+
 
     def to_dict(self):
         return {}
